79d84ddd1f5e91a25c2412d2ca138ccdc37337e7
JTonfire <letsslamdunk@gmail.com>
2016-09-29 18:49:40 -0400

    Finished game with a main menu and music during game and main menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public Button Play_Button;
+	public Button Quit_Button;
+	public Button Home_Button;
+
+
+	public void Play_Game () {
+		Debug.Log ("Play Game");
+		SceneManager.LoadScene (0);
+	}
+	public void Quit_Game() {
+
+		Application.Quit (); 
+	} 
+	public void Home () {
+		Debug.Log ("In Main");
+		SceneManager.LoadScene (1);
+	} 
+}
